fbef48e34e73b88a84ab66a8be1ae07fb513a565
Henri Gil <mohamed.denoun@ca-pacifica.fr>
2019-11-13 17:31:33 +0100

    Add fonction for parse all url of picture from html page

--- Main.c
+++ Main.c
@@ -181,39 +181,7 @@ int main(int argc, char* argv[]) {
 
 } 
 
-int Search_in_File(char *fname, char *str) {
-	FILE *fp;
-	int line_num = 1;
-	int find_result = 0;
-	char temp[512];
-    int cursor = 0;
-	
-	if((fp = fopen(fname, "r")) == NULL) {
-		return(-1);
-	}
 
-	while(fgets(temp, 512, fp) != NULL) {
-		if((strstr(temp, str)) != NULL) {
-			printf("A match found on line: %d\n", line_num);
-            printf("A match found on position: %d\n", cursor);
-			printf("%s\n", temp);
-			find_result++;
-            
-		}
-        cursor++;
-		line_num++;
-	}
-
-	if(find_result == 0) {
-		printf("\nSorry, couldn't find a match.\n");
-	}
-	
-	//Close the file if still open.
-	if(fp) {
-		fclose(fp);
-	}
-   	return(0);
-}
 
 struct MemoryStruct {
   char *memory;
@@ -317,6 +285,125 @@ int main(void)
   return 0;
 }
 */
+
+
+void Search_in_File(char *fname, char *str) {
+	FILE *fp;
+	int line_num = 1;
+	int find_result = 0;
+	char temp[512];
+    int cursor = 0;
+	
+	if((fp = fopen(fname, "r")) == NULL) {
+		return;
+	}
+
+	while(fgets(temp, 512, fp) != NULL) {
+		if((strstr(temp, str)) != NULL) {
+			printf("A match found on line: %d\n", line_num);
+            printf("A match found on position: %d\n", cursor);
+			printf("%s\n", temp);
+			find_result++;
+            
+		}
+        cursor++;
+		line_num++;
+	}
+
+	if(find_result == 0) {
+		printf("\nSorry, couldn't find a match.\n");
+	}
+	
+	//Close the file if still open.
+	if(fp) {
+		fclose(fp);
+	}
+}
+
+char* ImageFromHtml(char *fname) {
+	
+    char c;
+    char temp[5];
+    temp[4] = '\0';
+    int iTemp = 0;
+    char *pictureFname;
+    int pictureFnameSize = strlen( fname ) + 1 +  strlen( ".Link.txt" ) + 1; 
+    /* Memory allocation and copy */
+    pictureFname = (char *) malloc( pictureFnameSize );
+    strcpy( pictureFname, fname );
+    strcat( pictureFname, ".Link.txt" );
+    FILE *fpicture = fopen(pictureFname, "w");
+    FILE *fhtml = fopen(fname, "r");
+    
+	
+
+
+	if(fhtml != NULL){
+
+        int srct = 0;
+        int imgt = 0;
+        int carriageAdd = 0;
+        int domainAdd = 0;
+        c=fgetc(fhtml);
+        
+        while(c != EOF){
+            c=fgetc(fhtml);
+            if(iTemp < 4){
+                temp[iTemp] = c;
+                iTemp += 1;
+                
+            } else {
+              
+                
+                temp[0] = temp[1];
+                temp[1] = temp[2];
+                temp[2] = temp[3];
+                temp[3] = c;
+                iTemp += 1;
+            }
+
+            if(strcmp(temp,"<img") == 0){
+                imgt = 1;
+            }
+            if(imgt == 1) {
+                if(strcmp(temp," src") == 0){
+                    
+                    srct = 1;
+                    fseek(fhtml,2,SEEK_CUR);
+                    c=fgetc(fhtml);
+                }
+            }
+            if(imgt == 1 && srct == 1) {
+                
+                if(c=='"') {
+                    srct = 0;
+                    imgt = 0;
+                    carriageAdd = 1;
+                    continue;
+                }
+                fputc(c,fpicture);
+            }
+
+            if(carriageAdd == 1 && imgt == 0 && srct == 0) {
+                fputc('\n',fpicture);
+                carriageAdd = 0;
+            }
+
+
+
+
+        }
+        fclose(fhtml);
+        fclose(fpicture);
+        return pictureFname;
+
+    } 
+    fprintf(stderr, "File not fond. Check your file path");
+    return NULL;
+    
+}
+
+
 void HtmlDownload(char *url, char *outputFilePath) {
 
     CURL *curl;
@@ -338,14 +425,146 @@ void HtmlDownload(char *url, char *outputFilePath) {
         fclose(fp);
     }   
 }
+
+size_t write_data(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+    size_t written = fwrite(ptr, size, nmemb, stream);
+    return written;
+}
+
+char *CheckUrl(char *url, int size, char *domain, int domainSize) {
+
+    if(url[0] == '/') {
+        char *checkedUrl;
+        checkedUrl = malloc((size + domainSize));
+        checkedUrl[(size + domainSize)] = '\0';
+        strcpy(checkedUrl, domain);
+        strcpy(checkedUrl, url);
+        free(checkedUrl);
+        return checkedUrl;
+    }
+    if(url[size - 3] == ';') {
+        printf("yes");
+        char *checkedUrl;
+        checkedUrl = malloc((size - 2));
+        memcpy( checkedUrl, &url[0], (size-2) );
+        checkedUrl[(size-3)] = '\0';
+        free(checkedUrl);
+        return checkedUrl;
+    }
+    return url;
+
+}
+
+void DownloadImages(char *filePath, char* domain, int domainSize) {
+    CURL *curl;
+    FILE *fp;
+    CURLcode res;
+    char c;
+    char *outfilename = "/Users/mohamed.dennoun/Desktop/scrappeur/Websites/Google/saveimage.jpeg";
+    
+    
+
+    FILE* file = fopen(filePath,"r");
+    if(file != NULL){
+
+        double start = 0;
+        int size;
+        int nbUrl = 0;
+
+        
+
+        c=fgetc(file);
+        while((c)!=EOF){
+            
+            if(c =='\n') {
+                nbUrl += 1;
+
+            }
+            c=fgetc(file);
+        }
+        
+        rewind(file);
+
+
+        c=fgetc(file);
+        while((c)!=EOF){
+            
+            if(c =='\n') {
+                size = ftell(file) - start;
+                start =ftell(file);
+                break;
+
+            }
+            c=fgetc(file);
+        }
+        printf("%d\n", size);
+
+        int i = 0;
+        char url [size];
+        url[size] = '\0';
+        printf("%lu\n",sizeof(url));
+        rewind(file);
+        while(i < size && c !=EOF){
+            c = fgetc(file);
+            url[i] = c;
+            i += 1;
+        }
+
+        //tester
+        char *checkUrl = CheckUrl(url, size, domain, domainSize);
+
+        printf("%s\n",url);
+        printf("%s\n",checkUrl);
+        printf("%d\n", nbUrl);
+
+        curl = curl_easy_init();
+        if (curl) {
+        fp = fopen(outfilename,"wb");
+        curl_easy_setopt(curl, CURLOPT_URL, checkUrl);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        res = curl_easy_perform(curl);
+        /* always cleanup */
+        curl_easy_cleanup(curl);
+        fclose(fp);
+
+        }
+
+
+        
+        
+        
+
+        fclose(file);
+
+    } else {
+
+        fprintf(stderr, "File not fond. Check your file path");
+    }
+}
+
+
 int main(void)
 {
 
-    char *url = "https://fr.wikipedia.org/wiki/HTTP_301";
+    char *url = "https://www.google.com/search?hl=fr&biw=1200&bih=859&tbm=isch&sxsrf=ACYBGNRAMdspQa4mGT7-nOMeEU6zZ91eoA%3A1573652595473&sa=1&ei=cwjMXYG_HK-C1fAPxc686Ao&q=nature&oq=nature&gs_l=img.3..0l10.4362.6056..6249...1.0..0.119.388.6j1......0....1..gws-wiz-img.......35i39j0i67j0i131.euJCWi7oBlA&ved=0ahUKEwjBxM_fqOflAhUvQRUIHUUnD60Q4dUDCAc&uact=5";
     char *outputFilePath = "/Users/mohamed.dennoun/Desktop/scrappeur/Websites/Google/google.html";
     HtmlDownload(url,outputFilePath);
     char *cmd = "curl https://www.pexels.com/ | grep img | grep -o src=http.* | grep -o http.* | sed 's/\?.*//' > ./Websites/Google/googleLinks.txt";
-    system(cmd);
+    // system(cmd);
+
+    char *imageUrl = "https://unicornify.pictures/avatar/51d623f33f8b83095db84ff35e15dbe8?s=128";
+
+    char* pictureFname = ImageFromHtml(outputFilePath);
+    printf("%s\n", pictureFname);
+    if(pictureFname != NULL) {
+        DownloadImages(pictureFname, "", 0);
+    }
+    
+
+    
+
+    
     
     return 0;
 }
